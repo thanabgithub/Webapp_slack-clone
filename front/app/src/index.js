84b15fba8856be1bc094276ccf435b5a6788eeb5
Thana B <thana.b@outlook.jp>
2022-06-12 08:14:54 +0000

    update

--- front/app/src/index.js
+++ front/app/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,7 +12,9 @@ const Root = () => (
   <Router>
     <Routes>
       {/* old syntax was <Route path="/" component={App} /> */}
-      <Route path="/" element={<App />} />
+      <Route exact path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </Router>
 );
